7fa0ac75bb837635b879238407d0858dc3c471e0
devellen <ellenpatrycya66@gmail.com>
2022-12-07 22:10:19 -0300

    subindo atividade 2 - react native

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import { Avatar, Input, Button } from 'react-native-elements';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const Flex = () => {
+    return (
+        <View style={[styles.container, {
+            flexDirection: "column"
+        }]}>
+            <View style={{ flex: 4, alignItems: "center", paddingTop: 40 }}>
+                <Avatar
+                    avatarStyle={styles.avatar}
+                    size="xlarge"
+                    rounded
+                    source={{
+                        uri:
+                            'https://img2.gratispng.com/20180625/req/kisspng-computer-icons-avatar-business-computer-software-user-avatar-5b3097fcae25c3.3909949015299112927133.jpg',
+                    }}
+                />
+            </View>
+            <View style={{ flex: 2, padding: 20 }}>
+                <Input
+                    placeholder='Login'
+                />
+                <Input
+                    placeholder='Senha'
+                />
+            </View>
+            <View style={{ flex: 4, padding: 60 }}>
+                <Button
+                    buttonStyle={styles.button}
+                    title="Entrar"
+                />
+                <Button
+                    buttonStyle={styles.button}
+                    title="Cadastre-se"
+                />
+            </View>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        padding: 20
+    },
+    button: {
+        borderRadius: 40,
+        margin: 10,
+        backgroundColor: "#8f8fcb"
+    }
 });
+
+export default Flex;
